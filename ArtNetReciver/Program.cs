dfe64fe75ca56c00a5c31c1a76cb5bf1a1374831
xlinka <Oliver.dunnington@outlook.com>
2023-02-11 17:16:44 +0000

    Update Program.cs
    no clue why i did this

--- ArtNetReciver/Program.cs
+++ ArtNetReciver/Program.cs
@@ -25,38 +25,46 @@ class ArtNetServer
         Console.WriteLine("Art-Net server is running.");
 
         // Create a UDP client with the IP endpoint and port 0x1936
-        IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0x1936);
-        UdpClient udpClient = new UdpClient(remoteEP);
+        IPEndPoint remoteEP1 = new IPEndPoint(IPAddress.Any, 0x1936);
+        UdpClient udpClient1 = new UdpClient(remoteEP1);
+
+        IPEndPoint remoteEP2 = new IPEndPoint(IPAddress.Any, 8000);
+        UdpClient udpClient2 = new UdpClient(remoteEP2);
+
 
         // Loop to receive data
         while (true)
         {
-            // Receive data from the remote endpoint
-            byte[] data = udpClient.Receive(ref remoteEP);
-
+           
             // Get the universe from the received data
-            int universe = data[14] | (data[15] << 8);
-            universes[universe] = universe;
+            byte[] data1 = udpClient1.Receive(ref remoteEP1);
+            byte[] data2 = udpClient2.Receive(ref remoteEP2);
+
+            int universe1 = data1[14] | (data1[15] << 8);
+            int universe2 = data2[14] | (data2[15] << 8);
+
+            universes[universe1] = universe1;
+            universes[universe2] = universe2;
 
             // Handle Art-Net data
-            if (universe == 0x1936)
+            if (universe1 == 0x1936)
             {
                 // Extract the Art-Net data from the received data
-                byte[] artnetData = new byte[data.Length - 18];
-                Array.Copy(data, 18, artnetData, 0, artnetData.Length);
+                byte[] artnetData = new byte[data1.Length - 18];
+                Array.Copy(data1, 18, artnetData, 0, artnetData.Length);
 
                 // Convert the Art-Net data to hexadecimal string
                 string hexData = BitConverter.ToString(artnetData).Replace("-", "");
 
                 // Broadcast the Art-Net data to all clients connected to the /artnet service
                 wssv.WebSocketServices["/artnet"].Sessions.Broadcast(
-                    $"[{universe}, {hexData}]");
+                    $"[{universe1}, {hexData}]");
             }
             // Handle OSC data
-            else if (universe == 0x2300)
+            else if (universe2 == 0x2300)
             {
                 // Extract the OSC data from the received data
-                string oscData = System.Text.Encoding.UTF8.GetString(data, 18, data.Length - 18);
+                string oscData = System.Text.Encoding.UTF8.GetString(data2, 18, data2.Length - 18);
 
                 // Broadcast the OSC data to all clients connected to the /osc service
                 wssv.WebSocketServices["/osc"].Sessions.Broadcast(oscData);
