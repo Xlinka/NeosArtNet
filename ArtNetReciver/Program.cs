4edf2df6520d957ba4398a9e5f320c6f8668719e
xlinka <Oliver.dunnington@outlook.com>
2023-02-11 16:24:57 +0000

    OSC weh
    adding osc support

--- ArtNetReciver/Program.cs
+++ ArtNetReciver/Program.cs
@@ -27,13 +27,23 @@ class ArtNetServer
             int universe = data[14] | (data[15] << 8);
             universes[universe] = universe;
 
-            byte[] artnetData = new byte[data.Length - 18];
-            Array.Copy(data, 18, artnetData, 0, artnetData.Length);
+            // Handle Art-Net data
+            if (universe == 0x1936)
+            {
+                byte[] artnetData = new byte[data.Length - 18];
+                Array.Copy(data, 18, artnetData, 0, artnetData.Length);
 
-            string hexData = BitConverter.ToString(artnetData).Replace("-", "");
+                string hexData = BitConverter.ToString(artnetData).Replace("-", "");
 
-            wssv.WebSocketServices["/artnet"].Sessions.Broadcast(
-                $"[{universe}, {hexData}]");
+                wssv.WebSocketServices["/artnet"].Sessions.Broadcast(
+                    $"[{universe}, {hexData}]");
+            }
+            // Handle OSC data
+            else if (universe == 0x2300)
+            {
+                string oscData = System.Text.Encoding.UTF8.GetString(data, 18, data.Length - 18);
+                wssv.WebSocketServices["/osc"].Sessions.Broadcast(oscData);
+            }
         }
     }
 }
@@ -53,6 +63,24 @@ class ArtNetService : WebSocketBehavior
     protected override void OnMessage(MessageEventArgs e)
     {
         Console.WriteLine("A WebSocket client sent a message: " + e.Data);
+        Send(e.Data);
+    }
+}
+class OSCService : WebSocketBehavior
+{
+    protected override void OnOpen()
+    {
+        Console.WriteLine("A WebSocket client connected to the /osc service.");
+    }
+
+    protected override void OnClose(CloseEventArgs e)
+    {
+        Console.WriteLine("A WebSocket client disconnected from the /osc service.");
+    }
+
+    protected override void OnMessage(MessageEventArgs e)
+    {
+        Console.WriteLine("A WebSocket client sent a message to the /osc service: " + e.Data);
         Send(e.Data);
     }
 }
